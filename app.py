8cd6e276bf22e64323f8a06fd085d2731cf4d3ee
Starlone <starlone.passos@gmail.com>
2020-08-21 15:19:06 -0300

    Sort files

--- app.py
+++ app.py
@@ -5,16 +5,28 @@ import sys
 import git
 
 
+def of_sort_files_type(file):
+    return file.type
+
+
+def of_sort_files_path(file):
+    return file.path
+
+
 class OfManager:
     def __init__(self, path):
         self.repo = git.Repo(path)
         self.files = []
+
+        name = self.repo.git.working_dir
+        self.name = name.split('/')[-1]
 
     def find_commits(self, **params):
         commits = []
         for commit in self.repo.iter_commits(**params):
             ofcommit = OfCommit(commit)
             commits.append(ofcommit)
+
         return commits
 
     def join_commits(self, **params):
@@ -27,12 +39,24 @@ class OfManager:
         for ofcommit in commits:
             for offile in ofcommit.files:
                 old = paths.get(offile.path)
-                if not old:
+                if old:
+                    if offile.type == 'A':
+                        files.remove(old)
+                        files.append(offile)
+                        paths[offile.path] = offile
+                else:
                     if offile.type == 'R':
                         paths[offile.path_old] = offile
                     paths[offile.path] = offile
                     files.append(offile)
+        files.sort(key=of_sort_files_path)
+        files.sort(key=of_sort_files_type)
         return files
+
+    def file_to_str(self, offile):
+        if offile.type == 'M':
+            return self.name + '/' + offile.path
+        return offile.type + ' ' + self.name + '/' + offile.path
 
 
 class OfCommit:
@@ -47,6 +71,9 @@ class OfCommit:
         for diff in diffs:
             offile = OfFile(diff)
             self.files.append(offile)
+
+        self.files.sort(key=of_sort_files_path)
+        self.files.sort(key=of_sort_files_type)
 
 
 class OfFile:
@@ -71,7 +98,6 @@ if __name__ == "__main__":
             projects = sys.argv[2:]
 
     for project in projects:
-        print(project)
         manager = OfManager(project)
 
         params = {
@@ -79,12 +105,15 @@ if __name__ == "__main__":
             'no_merges': True
         }
 
-        print("Commits")
-        for commit in manager.find_commits(**params):
-            print(commit.message)
-            for offile in commit.files:
-                print(offile)
+        commits = manager.find_commits(**params)
+        if commits:
+            print('\n ##' + project)
+            print("Commits\n")
+            for commit in commits:
+                print(commit.message)
+                for offile in commit.files:
+                    print(manager.file_to_str(offile))
 
-        print("\n\nJoin Commits")
-        for offile in manager.join_commits(**params):
-            print(offile)
+            print("\n\nJoin Commits\n")
+            for offile in manager.join_commits(**params):
+                print(manager.file_to_str(offile))
