9c27d18a58ec5fe3333c39fb454a90209645be26
Starlone <starlone.passos@gmail.com>
2022-07-18 10:07:51 -0300

    feat: create and request api

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 import os
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-index_html_content = ""
-with open("templates/index.html", mode="r") as f:
-    index_html_content = f.read()
+
+class ApiBody(BaseModel):
+    project: str
+    task: str
 
 
 @app.get("/", response_class=HTMLResponse)
 async def index():
+    index_html_content = get_index_html()
     return HTMLResponse(content=index_html_content, status_code=200)
+
+
+@app.post("/api")
+async def api(payload: ApiBody):
+    print(payload)
+    return {
+        "message": 'oi'
+    }
+
+
+def get_index_html():
+    index_html_content = ""
+    with open("templates/index.html", mode="r") as f:
+        index_html_content = f.read()
+    return index_html_content
+
 
 DEV = os.getenv("DEV", "false") == 'true'
 
